8674bebe67b03ea81cf4aab362aaa0e5ae58854e
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-01-06 17:56:44 -0500

    Making sure Luciernaga only contains functions that are generalizable is going to take a bit of work.

--- R/Utility_Subtract.R
+++ R/Utility_Subtract.R
@@ -2,14 +2,18 @@
 #'
 #' @param i A row of the data.frame object
 #' @param Data A data.frame object
+#' @import dplyr
 #'
 #' @return NULL
-#' @export NULL
+#' @export
+#'
 #'
 #' @examples NULL
 Utility_Subtract <- function(i, Data){
+  #Generalization Note: This one may be a lot of effort generalize, most of it is specific.
+
   Z <- Data[i,]
-  Alpha <- Data %>% dplyr::filter(Experiment == Z$Experiment) %>% dplyr::filter(Type == Z$Type) %>% dplyr::filter(Negative == Z$Negative) %>% dplyr::filter(str_detect(Ligand, "Unstained"))
+  Alpha <- Data %>% filter(Experiment == Z$Experiment) %>% filter(Type == Z$Type) %>% filter(Negative == Z$Negative) %>% filter(str_detect(Ligand, "Unstained"))
   Subtracted <- Z[,6:69] - Alpha[,6:69]
   Updated <- cbind(Z[,1:5], Subtracted)
   Updated %>% mutate(Processed = "Yes") %>% relocate(Processed, .after = Negative)
