c7fe50a8d61da6dd8d382a50ac040db702b6d47a
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-01-31 18:29:06 -0500

    Added help landing page and a citation.

---
+++ R/Luciernaga-package.R
@@ -0,0 +1,8 @@
+#' @description
+#' To learn more about how to use Luciernaga, please start by browsing the vignettes:
+#'#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
