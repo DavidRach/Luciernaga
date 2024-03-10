84f8f2439518bf1cd50be30865a7167aa3f9005e
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-03-10 18:29:09 -0400

    As far as I got today

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,3 +1,3 @@
 #' @importFrom basilisk BasiliskEnvironment
 env1 <- BasiliskEnvironment("env1", pkgname="Luciernaga",
-                            packages=c("pandas", "pacmap"))
+                            packages=c("pandas==2.2.1", "numpy==1.26.4", "pacmap==0.7.2"))
