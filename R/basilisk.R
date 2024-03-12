8a224de541535d21c3ee2d312a374a6bf17740c7
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-03-11 22:33:26 -0400

    UMAP is now operational. Only have to figure out how to deal with the opt-SNE Python wrapper majig. Because that is likely to be one first critiques?

--- R/basilisk.R
+++ R/basilisk.R
@@ -4,3 +4,4 @@ env1 <- BasiliskEnvironment("env1", pkgname="Luciernaga",
 
 env2 <- BasiliskEnvironment("env2", pkgname="Luciernaga",
                             packages=c("pandas==2.2.1", "numpy==1.26.4", "phate==1.0.11"))
+
