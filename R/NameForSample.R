c25855bc0441cc07a1fbb65983968ec7239db45d
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-08-04 18:14:22 -0400

    Moved the name experiment and condition to it's own function, and figured out basics of ... argument passing

---
+++ R/NameForSample.R
@@ -0,0 +1,45 @@
+#' Generate a name from the sample for plot identification
+#'
+#' @param x A GatingSet object
+#' @param sample.name The .fcs keyword that contains an unique name for that sample
+#' @param removestrings A string of character values to remove from the sample.name
+#' @param experiment.name The .fcs keyword that contains the name of the experiment
+#' @param experiment Directly provide the name of the experiment (alternative to experiment.name)
+#' @param condition.name The .fcs keyword that contains the name of the condition.
+#' @param condition Directly provide the name of the condition (alternative to condition.name)
+#'
+#' @importFrom flowWorkspace keyword
+#'
+#' @return A name matching the provided information for use in plot labeling.
+#' @export
+#'
+#' @examples NULL
+
+NameForSample <- function(x, sample.name, removestrings, experiment.name = NULL,
+                          experiment = NULL, condition.name = NULL, condition = NULL){
+
+  name <- keyword(x, sample.name)
+  name <- NameCleanUp(name = name, removestrings)
+
+  if (exists("experiment")){experiment <- experiment
+  } else if (exists("experiment.name")){
+     if(!is.null(experiment.name)){experiment <- keyword(x, experiment.name)}
+   } else {experiment <- NULL}
+
+  if (exists("condition")){condition <- condition
+  } else if (exists("condition.name")){
+    if(!is.null(condition.name)){condition <- keyword(x, condition.name)}
+  } else {condition <- NULL}
+
+  name
+  experiment
+  condition
+
+  if (is.null(experiment) && is.null(condition)){AggregateName <- name}
+  if (!is.null(experiment) && is.null(condition)){AggregateName <- paste(AggregateName, experiment, sep="_")}
+  if (is.null(experiment) && !is.null(condition)){AggregateName <- paste(AggregateName, condition, sep="_")}
+  if (!is.null(experiment) && !is.null(condition)){AggregateName <- paste(AggregateName, experiment, condition, sep="_")}
+
+  return(AggregateName)
+}
+
--- R/Utility_GatingPlots.R
+++ R/Utility_GatingPlots.R
@@ -1,31 +1,35 @@
-#' Check gate placement for individual .fcs files
+#' Check gate placement for individual .fcs files in a GatingSet
 #'
-#' @param x A Gating Set object
-#' @param sample.name The .fcs keyword containing the samples name
-#' @param removestrings The values you want removed from sample.name
-#' @param experiment Provide an alternative experiment name
-#' @param experiment.name The .fcs keyword containing the experiment value
-#' @param condition Provide an alternative condition name
-#' @param condition.name The .fcs keyword containing the condition name
-#' @param subsets The subset level you want the data plotted at. "root" as default
-#' @param gtFile The imported data.table containing the gate information.
-#' @param column Residual superceeded
-#' @param bins Number of geom hexbins to bin the plotted data for visualization.
+#' @param x A GatingSet object
+#' @param sample.name The .fcs keyword that contains an unique name for that sample
+#' @param removestrings A string of character values to remove from the sample.name
+#' @param experiment.name The .fcs keyword that contains the name of the experiment
+#' @param experiment Directly provide the name of the experiment (alternative to experiment.name)
+#' @param condition.name The .fcs keyword that contains the name of the condition.
+#' @param condition Directly provide the name of the condition (alternative to condition.name)
+#' @param subsets The GatingSet subset that you want to visualize for data plotting, "root" is the default.
+#' @param bins Argument to geom_hex for number of bins to visualize the plotted data density.
+#' @param gtFile The data.table imported .csv file containing the gating template.
+#' @param DesiredGates A vector of gates that you want plotted, for example Desired <- c("nonDebris, "lymphocytes")
+#' @param export Whether to return as a .pdf file or as individual patchwork ggplot objects.
+#' @param thecolumns Number of desired columns for the .pdf file
+#' @param therows Number of desired rows for the .pdf file
+#' @param outpath Location to store the generated .pdf file
+#'
 #' @param clearance Area around edge
-#' @param outpath The location to send the generated .pdf file
-#' @param export Whether to export as a .pdf file to the outpath, TRUE, FALSE
 #'
 #' @importFrom flowWorkspace keyword
 #' @importFrom flowWorkspace gs_pop_get_data
 #' @importFrom flowCore exprs
-#' @importFrom dplyr filter
 #' @importFrom dplyr pull
+#' @importFrom purrr map
+#'
+#' @importFrom dplyr filter
 #' @importFrom dplyr select
 #' @importFrom stringr str_split
 #' @importFrom ggcyto as.ggplot
 #' @importFrom ggcyto ggcyto
 #' @import ggplot2
-#' @importFrom purrr map
 #' @importFrom patchwork wrap_plots
 #' @importFrom grDevices dev.off
 #' @importFrom grDevices pdf
@@ -36,100 +40,34 @@
 #'
 #' @examples NULL
 
-Utility_GatingPlots <- function(x, sample.name, removestrings,
-    experiment = NULL, experiment.name = NULL, condition = NULL,
-    condition.name = NULL, subsets, gtFile, column, bins, clearance,
-    outpath, export = NULL){
+Utility_GatingPlots <- function(x, sample.name, removestrings, subsets="root", gtFile,
+    DesiredGates = NULL, thecolumn = 2, therows = 2, bins, clearance, outpath = NULL, export = NULL, ...){
 
-  name <- keyword(x, sample.name)
-  name <- NameCleanUp(name = name, removestrings)
+  # Setting up individual file name
+  if (is.null(outpath)){outpath <- getwd()}
 
-  if(exists("experiment")) {experiment <- experiment
-  } else if (exists("experiment.name")) {experiment <- keyword(x, experiment.name)
-  } else {experiment <- NULL}
-
-  if(exists("condition")){condition <- condition
-  } else if (exists("condition.name")) {condition <- keyword(x, condition.name)
-  } else {condition <- NULL}
-
-  if (!is.null(experiment)){
-    AggregateName <- paste0(name, experiment)
-  } else {AggregateName <- name}
+  AggregateName <- NameForSample(x=x, sample.name=sample.name, removestrings=removestrings, ...)
 
   StorageLocation <- file.path(outpath, AggregateName)
 
-  ff <- gs_pop_get_data(x, subsets)
-  df <- exprs(ff[[1]]) #Is the one necessary in this case? Unclear
-  #how works with lapply...
-  TheDF <- data.frame(df, check.names = FALSE)
-
+  # Pulling Gating Information
   TheXYZgates <- gtFile %>% pull(alias)
 
+  # Desired Gate
+
+
+  # Pulling Gating Set Data
+  ff <- gs_pop_get_data(x, subsets)
+  df <- exprs(ff[[1]])
+  TheDF <- data.frame(df, check.names = FALSE)
+
   x2 <- x
-
-  GatePlot <- function(x, gtFile){
-    i <- x
-    gtFile <- data.frame(gtFile, check.names = FALSE)
-    RowData <- gtFile %>% dplyr::filter(alias %in% i)
-    theSubset <- RowData %>% pull(parent)
-    theGate <- RowData %>% pull(alias)
-    theParameters <- RowData %>% pull(dims) %>% str_split(",", simplify = TRUE)
-
-    theParameters <- gsub("^\\s+|\\s+$", "", theParameters)
-
-    if(length(theParameters) == 2){xValue <- theParameters[[1]]
-    yValue <- theParameters[[2]]
-    } else if (length(theParameters) == 1){xValue <- theParameters[[1]]
-    yValue <- "SSC-A" #or an alternate variable specify
-    } else {message(
-      "Plotting Parameters for Axis were not 1 or 2, please check the .csv file")}
-
-
-    #Please Note, All the Below Are Raw Values With No Transforms Yet Applied.
-
-    if (!grepl("FSC|SSC", xValue)) {ExprsData <- TheDF %>%
-      select(all_of(xValue)) %>% pull()
-    theXmin <- ExprsData %>% quantile(., 0.001)
-    theXmax <- ExprsData %>% quantile(., 0.999)
-    theXmin <- theXmin - abs((clearance*theXmin))
-    theXmax <- theXmax + (clearance*theXmax)}
-    if (!grepl("FSC|SSC", yValue)) {ExprsData <- TheDF %>%
-      select(all_of(yValue)) %>% pull()
-    theYmin <- ExprsData %>% quantile(., 0.001)
-    theYmax <- ExprsData %>% quantile(., 0.999)
-    theYmin <- theYmin - abs((clearance*theYmin))
-    theYmax <- theYmax + (clearance*theYmax)}
-
-    if (!exists("theYmax") || !exists("theXmax")){
-      Plot <- as.ggplot(ggcyto(x2, aes(x = .data[[xValue]], y = .data[[yValue]]),
-         subset = theSubset) + geom_hex(bins=bins) + geom_gate(theGate) +
-           theme_bw() + labs(title = NULL) + theme(
-             strip.background = element_blank(), strip.text.x = element_blank(),
-             panel.grid.major = element_line(linetype = "blank"),
-             panel.grid.minor = element_line(linetype = "blank"),
-             axis.title = element_text(size = 10, face = "bold"),
-             legend.position = "none"))
-
-    } else {Plot <- as.ggplot(ggcyto(x2, aes(
-      x = .data[[xValue]], y = .data[[yValue]]), subset = theSubset) +
-        geom_hex(bins=bins) + coord_cartesian(xlim = c(theXmin, theXmax),
-       ylim = c(theYmin, theYmax), default = TRUE) + geom_gate(theGate) +
-        theme_bw() + labs(title = NULL) + theme(strip.background = element_blank(),
-       strip.text.x = element_blank(), panel.grid.major = element_line(
-         linetype = "blank"), panel.grid.minor = element_line(
-           linetype = "blank"), axis.title = element_text(size = 10,
-                                                          face = "bold"),
-                                      legend.position = "none"))
-
-    tryCatch({rm("theXmin", "theXmax", "theYmin", "theYmax")})}
-  }
-
   CompiledPlots <- map(.x = TheXYZgates, .f = GatePlot, gtFile = gtFile)
 
   theList <- CompiledPlots
   theListLength <- length(theList)
 
-  thecolumns <- 2
+  thecolumns
   therows <- 3
   theoreticalitems <- therows*thecolumns
 
@@ -153,4 +91,70 @@ Utility_GatingPlots <- function(x, sample.name, removestrings,
   dev.off()
 
   } else {return(AssembledPlots)}
+}
+
+
+
+
+
+
+
+
+
+
+GatePlot <- function(x, gtFile){
+  i <- x
+  gtFile <- data.frame(gtFile, check.names = FALSE)
+  RowData <- gtFile %>% dplyr::filter(alias %in% i)
+  theSubset <- RowData %>% pull(parent)
+  theGate <- RowData %>% pull(alias)
+  theParameters <- RowData %>% pull(dims) %>% str_split(",", simplify = TRUE)
+
+  theParameters <- gsub("^\\s+|\\s+$", "", theParameters)
+
+  if(length(theParameters) == 2){xValue <- theParameters[[1]]
+  yValue <- theParameters[[2]]
+  } else if (length(theParameters) == 1){xValue <- theParameters[[1]]
+  yValue <- "SSC-A" #or an alternate variable specify
+  } else {message(
+    "Plotting Parameters for Axis were not 1 or 2, please check the .csv file")}
+
+
+  #Please Note, All the Below Are Raw Values With No Transforms Yet Applied.
+
+  if (!grepl("FSC|SSC", xValue)) {ExprsData <- TheDF %>%
+    select(all_of(xValue)) %>% pull()
+  theXmin <- ExprsData %>% quantile(., 0.001)
+  theXmax <- ExprsData %>% quantile(., 0.999)
+  theXmin <- theXmin - abs((clearance*theXmin))
+  theXmax <- theXmax + (clearance*theXmax)}
+  if (!grepl("FSC|SSC", yValue)) {ExprsData <- TheDF %>%
+    select(all_of(yValue)) %>% pull()
+  theYmin <- ExprsData %>% quantile(., 0.001)
+  theYmax <- ExprsData %>% quantile(., 0.999)
+  theYmin <- theYmin - abs((clearance*theYmin))
+  theYmax <- theYmax + (clearance*theYmax)}
+
+  if (!exists("theYmax") || !exists("theXmax")){
+    Plot <- as.ggplot(ggcyto(x2, aes(x = .data[[xValue]], y = .data[[yValue]]),
+                             subset = theSubset) + geom_hex(bins=bins) + geom_gate(theGate) +
+                        theme_bw() + labs(title = NULL) + theme(
+                          strip.background = element_blank(), strip.text.x = element_blank(),
+                          panel.grid.major = element_line(linetype = "blank"),
+                          panel.grid.minor = element_line(linetype = "blank"),
+                          axis.title = element_text(size = 10, face = "bold"),
+                          legend.position = "none"))
+
+  } else {Plot <- as.ggplot(ggcyto(x2, aes(
+    x = .data[[xValue]], y = .data[[yValue]]), subset = theSubset) +
+      geom_hex(bins=bins) + coord_cartesian(xlim = c(theXmin, theXmax),
+                                            ylim = c(theYmin, theYmax), default = TRUE) + geom_gate(theGate) +
+      theme_bw() + labs(title = NULL) + theme(strip.background = element_blank(),
+                                              strip.text.x = element_blank(), panel.grid.major = element_line(
+                                                linetype = "blank"), panel.grid.minor = element_line(
+                                                  linetype = "blank"), axis.title = element_text(size = 10,
+                                                                                                 face = "bold"),
+                                              legend.position = "none"))
+
+  tryCatch({rm("theXmin", "theXmax", "theYmin", "theYmax")})}
 }
