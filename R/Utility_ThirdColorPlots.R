1d5b40de1d4c00046605ec3ae7b076caef36f40b
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-08-06 11:01:24 -0400

    Start of the considerations for how to go about the third (etc...) parameter color highlighting for plots.

--- R/Utility_ThirdColorPlots.R
+++ R/Utility_ThirdColorPlots.R
@@ -1,3 +1,13 @@
-Utility_ThirdColorPlots <- function(x, xaxis, yaxis, zaxis){
-  print("Hello")
+Utility_ThirdColorPlots <- function(x, subset, xaxis, yaxis, zaxis, sample.name, removestrings){
+
+  AggregateName <- NameForSample(x=x, sample.name=sample.name, removestrings=removestrings, ...)
+  #StorageLocation <- file.path(outpath, AggregateName)
+
+  # Retrieving margin info for the x specimen
+  Margin <- gs_pop_get_data(x, subset)
+  df <- exprs(Margin[[1]])
+  TheDF <- data.frame(df, check.names = FALSE)
+
+
+
 }
