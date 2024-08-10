8e76beada6452a1901a95922e75bdee83b54951f
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-08-10 10:25:25 -0400

    Something about experiment = NULL taking preferance?

--- R/NameForSample.R
+++ R/NameForSample.R
@@ -9,6 +9,7 @@
 #' @param condition Directly provide the name of the condition (alternative to condition.name)
 #'
 #' @importFrom flowWorkspace keyword
+#' @importFrom dplyr pull
 #'
 #' @return A name matching the provided information for use in plot labeling.
 #' @keywords internal
@@ -19,26 +20,28 @@ NameForSample <- function(x, sample.name, removestrings, experiment.name = NULL,
                           experiment = NULL, condition.name = NULL, condition = NULL){
 
   name <- keyword(x, sample.name)
-  name <- NameCleanUp(name = name, removestrings)
+  name <- Luciernaga:::NameCleanUp(name = name, removestrings)
 
   if (exists("experiment")){experiment <- experiment
   } else if (exists("experiment.name")){
-     if(!is.null(experiment.name)){experiment <- keyword(x, experiment.name)}
+     if(!is.null(experiment.name)){theexperiment <- keyword(x, experiment.name)
+                                   experiment <- pull(experiment)}
    } else {experiment <- NULL}
 
   if (exists("condition")){condition <- condition
   } else if (exists("condition.name")){
-    if(!is.null(condition.name)){condition <- keyword(x, condition.name)}
+    if(!is.null(condition.name)){condition <- keyword(x, condition.name)
+                                 condition <- pull(condition)}
   } else {condition <- NULL}
 
-  name
-  experiment
-  condition
+  message(name)
+  message(experiment)
+  message(condition)
 
-  if (is.null(experiment) && is.null(condition)){AggregateName <- name}
-  if (!is.null(experiment) && is.null(condition)){AggregateName <- paste(name, experiment, sep="_")}
-  if (is.null(experiment) && !is.null(condition)){AggregateName <- paste(name, condition, sep="_")}
-  if (!is.null(experiment) && !is.null(condition)){AggregateName <- paste(name, experiment, condition, sep="_")}
+  if (is.null(experiment) && is.null(condition)){AggregateName <- name
+  } else if (!is.null(experiment) && is.null(condition)){AggregateName <- paste(name, experiment, sep="_")
+  } else if (is.null(experiment) && !is.null(condition)){AggregateName <- paste(name, condition, sep="_")
+  } else if (!is.null(experiment) && !is.null(condition)){AggregateName <- paste(name, experiment, condition, sep="_")}
 
   return(AggregateName)
 }
