f361929b343d864e5ec73ebfa7b53073f689fda9
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-10-01 13:11:27 -0400

    QC_ProspectiveAdditions has been integrated into Luciernaga. Needs the returnAll = TRUE filtering to be adjusted (not intelligible order currently).
    Added the required importFrom ggplot2 arguments to allow for QC_SimilarFluorophores to load from the get go without requiring a library(ggplot2).

---
+++ R/QC_ProspectiveAdditions.R
@@ -0,0 +1,176 @@
+#' From existing panel, figures out open detectors, and returns potential
+#'  fluorophores that might fit between the existing ones
+#'
+#' @param path Filepath to the panel .csv
+#' @param NumberDetectors Number of detectors for the instrument to pull references
+#' @param TheCutoff Default is 0.9, cosine matrix value
+#' @param returnAll Whether to return all variants, default is FALSE
+#' @param filename Desired name for the output .csv
+#' @param outpath Path Location to store the the output .csv
+#'
+#' @importFrom utils read.csv
+#' @importFrom dplyr pull
+#' @importFrom dplyr filter
+#' @importFrom dplyr group_by
+#' @importFrom dplyr arrange
+#' @importFrom dplyr desc
+#' @importFrom dplyr slice
+#' @importFrom dplyr select
+#' @importFrom dplyr ungroup
+#' @importFrom dplyr relocate
+#' @importFrom purrr map
+#' @importFrom dplyr bind_rows
+#' @importFrom utils write.csv
+#'
+#' @return A csv containing selected fluorophores
+#' @export
+#'
+#' @examples NULL
+QC_ProspectiveAdditions <- function(path, NumberDetectors, TheCutoff=0.9,
+                                    returnAll=FALSE, filename, outpath){
+
+  TheList <- read.csv(path, check.names=FALSE)
+  ReferenceData <- InstrumentReferences(NumberDetectors=NumberDetectors)
+
+  TheList <- TheList %>% pull(Fluorophore)
+  #x <- TheList[1]
+
+  TheReferenceList <- ReferenceData %>% dplyr::filter(Fluorophore %in% TheList)
+
+  TheDetectorContained <- TheReferenceList %>% group_by(Fluorophore) %>%
+    arrange(desc(AdjustedY)) %>% slice(1) %>% select(Fluorophore, Detector) %>%
+    ungroup()
+
+  TheOccupiedDetectors <- TheDetectorContained %>% pull(Detector) %>% sort()
+
+  TheOtherFluors <-  ReferenceData %>% dplyr::filter(!Fluorophore %in% TheList)
+  TheOtherDetectors <- TheOtherFluors %>% group_by(Fluorophore) %>%
+    arrange(desc(AdjustedY)) %>% slice(1) %>% select(Fluorophore, Detector) %>%
+    ungroup()
+  TheOtherDetectors <- TheOtherDetectors %>% arrange(Detector)
+  TheOtherDetectors <- TheOtherDetectors %>%
+    dplyr::filter(!Detector %in% TheOccupiedDetectors)
+
+  TheOtherDetectors <- TheOtherDetectors %>% relocate(Detector, .before=Fluorophore)
+
+  PossibleDetectors <- TheOtherDetectors %>% pull(Detector) %>%
+    unique() #%>% length()
+
+  DataUnlocked <- map(.x=PossibleDetectors, .f=Comparison,
+                      TheOtherDetectors=TheOtherDetectors,
+                      TheList=TheList, ReferenceData=ReferenceData,
+                      TheCutoff=TheCutoff) %>% bind_rows()
+
+  if (returnAll == TRUE){PossibleLocations <- DataUnlocked %>%
+    group_by(TheDetector) %>% arrange(RankValue) %>% ungroup()
+  } else {PossibleLocations <- DataUnlocked %>% group_by(TheDetector) %>%
+    arrange(RankValue) %>% slice(1) %>% ungroup()
+  }
+
+  TheOutput <- PossibleLocations %>% arrange(RankValue)
+
+  TheFileName <- paste0(filename, ".csv")
+
+  StoreHere <- file.path(outpath, TheFileName)
+  write.csv(TheOutput, StoreHere, row.names=FALSE)
+}
+
+
+#' Internal for QC_ProspectiveAdditions
+#'
+#' @param x Passed Argument
+#' @param TheOtherDetectors Passed Argument
+#' @param TheList Passed Argument
+#' @param ReferenceData Passed Argument
+#' @param TheCutoff Passed Argument
+#'
+#' @importFrom dplyr filter
+#' @importFrom dplyr pull
+#' @importFrom purrr map
+#' @importFrom dplyr bind_rows
+#'
+#' @noRd
+Comparison <- function(x, TheOtherDetectors, TheList, ReferenceData, TheCutoff){
+  TheDetector <- x
+
+  TheComparisonList <- TheOtherDetectors %>% dplyr::filter(Detector %in% x) %>%
+    pull(Fluorophore)
+
+  TheIndividualDetector <- map(.x=TheComparisonList, .f=InternalComparison,
+                               TheList=TheList, ReferenceData=ReferenceData,
+                               TheCutoff=TheCutoff, TheDetector=TheDetector) %>%
+    bind_rows()
+
+  return(TheIndividualDetector)
+}
+
+
+
+
+#' Internal for QC_ProspectiveAdditions
+#'
+#' @param x Passed Argument
+#' @param TheList Passed Argument
+#' @param ReferenceData Passed Argument
+#' @param TheCutoff Passed Argument
+#' @param TheDetector Passed Argument
+#'
+#' @importFrom dplyr filter
+#' @importFrom dplyr select
+#' @importFrom tidyr pivot_wider
+#' @importFrom dplyr pull
+#' @importFrom lsa cosine
+#' @importFrom tidyselect all_of
+#' @importFrom dplyr sym
+#'
+#' @noRd
+InternalComparison <- function(x, TheList, ReferenceData, TheCutoff, TheDetector){
+
+  TheCandidate <- ReferenceData %>% dplyr::filter(Fluorophore %in% x)
+  TheReferenceList <- ReferenceData %>% dplyr::filter(Fluorophore %in% TheList)
+  TheData <- rbind(TheCandidate, TheReferenceList) %>% select(-Instrument)
+  TheCosineData <- TheData %>%
+    pivot_wider(names_from = Detector, values_from = AdjustedY)
+  Names <- TheCosineData %>% pull(Fluorophore)
+  TheValues <- TheCosineData %>% select(-Fluorophore)
+  TheTransposed <- t(TheValues)
+  colnames(TheTransposed) <- Names
+  TheMatrix <- data.matrix(TheTransposed)
+  CosineMatrix <- cosine(TheMatrix)
+  CosineMatrix <- data.frame(CosineMatrix, check.names = FALSE)
+  TheCandidateValues <- CosineMatrix %>% select(all_of(x))
+  HighOverlaps <- TheCandidateValues %>% filter(!!sym(x) >= TheCutoff)
+  HighOverlaps <- length(HighOverlaps)
+  RankValue <- round(kappa(TheMatrix),2)
+  Fluorophore <- x
+
+  Prelim <- cbind(Fluorophore, TheDetector, HighOverlaps, RankValue)
+  Data <- data.frame(Prelim, check.names = FALSE)
+  return(Data)
+}
+
+#' Internal for QC_ProspectiveAdditions
+#'
+#' @param NumberDetectors A number corresponding to the number of detectors for
+#' the spectral instrument
+#'
+#' @importFrom utils read.csv
+#'
+#' @return The stored reference data for that instrument contained within
+#' Luciernaga
+#' @noRd
+InstrumentReferences <- function(NumberDetectors){
+  if (NumberDetectors == 64){instrument <- "FiveLaser"
+  FileLocation <- system.file("extdata", package = "Luciernaga")
+  TheFile <- file.path(FileLocation, "CytekReferenceLibrary5L.csv")
+  ReferenceData <- read.csv(TheFile, check.names = FALSE)
+  } else if (NumberDetectors == 54){instrument <- "FourLaser"
+  FileLocation <- system.file("extdata", package = "Luciernaga")
+  TheFile <- file.path(FileLocation, "CytekReferenceLibrary4LUV.csv")
+  ReferenceData <- read.csv(TheFile, check.names = FALSE)
+  } else if (NumberDetectors == 38){instrument <- "ThreeLaser"
+  FileLocation <- system.file("extdata", package = "Luciernaga")
+  TheFile <- file.path(FileLocation, "CytekReferenceLibrary3L.csv")
+  ReferenceData <- read.csv(TheFile, check.names = FALSE)
+  } else {message("No References Found")}
+}
--- R/QC_SimilarFluorophores.R
+++ R/QC_SimilarFluorophores.R
@@ -88,6 +88,15 @@ QC_SimilarFluorophores <- function(TheFluorophore, NumberDetectors, NumberHits, 
 #' @importFrom dplyr filter
 #' @importFrom dplyr rename
 #' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 geom_line
+#' @importFrom ggplot2 theme_bw
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 geom_hline
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 element_text
+#' @importFrom ggplot2 element_blank
+#' @importFrom ggplot2 scale_x_discrete
 #'
 #' @noRd
 SimilarFluorPlots <- function(TheseFluorophores, TheFluorophore, data){
