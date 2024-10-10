60d5638f1e5813a205846c989e321095b500d60d
David Rach <91153676+DavidRach@users.noreply.github.com>
2024-10-09 23:44:06 -0400

    26/43, first three vignettes transferred to example/unit tests [exception for the two basilisk dimviz functions].

--- R/Utility_tSNE.R
+++ R/Utility_tSNE.R
@@ -12,7 +12,7 @@
 #' @param max_iter Rtsne argument, default is set to 1000
 #' @param perplexity Rtsne argument, default is set to 30
 #' @param ... Other Arguments to pass to Rtsne
-#' @param export When set to TRUE returns fcs files to specified outpath.
+#' @param export When set to TRUE returns as fcs files to specified outpath.
 #'
 #' @importFrom Rtsne Rtsne
 #' @importFrom flowWorkspace keyword
@@ -23,10 +23,47 @@
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
 #'
-#' @return Numbers
+#' @return tSNE axes bound to a flowframe or fcs file
 #' @export
 #'
-#' @examples NULL
+#' @examples
+#'
+#' library(BiocGenerics)
+#' library(flowCore)
+#' library(flowWorkspace)
+#' library(openCyto)
+#' library(data.table)
+#'
+#' File_Location <- system.file("extdata", package = "Luciernaga")
+#' FCS_Files <- list.files(path = File_Location, pattern = ".fcs",
+#'   full.names = TRUE)
+#' Unmixed_FullStained <- FCS_Files[grep("Unmixed", FCS_Files)]
+#' UnmixedFCSFiles <- Unmixed_FullStained[1]
+#' UnmixedCytoSet <- load_cytoset_from_fcs(UnmixedFCSFiles[1],
+#'   truncate_max_range = FALSE,transformation = FALSE)
+#' UnmixedGatingSet <- GatingSet(UnmixedCytoSet)
+#' Markers <- colnames(UnmixedCytoSet)
+#' KeptMarkers <- Markers[-grep("Time|FS|SC|SS|Original|-W$|-H$|AF", Markers)]
+#' MyBiexponentialTransform <- flowjo_biexp_trans(channelRange = 256,
+#'   maxValue = 1000000,pos = 4.5, neg = 0, widthBasis = -1000)
+#' TransformList <- transformerList(KeptMarkers, MyBiexponentialTransform)
+#' UnmixedGatingSet <- flowWorkspace::transform(UnmixedGatingSet, TransformList)
+#' FileLocation <- system.file("extdata", package = "Luciernaga")
+#' UnmixedGates <- fread(file.path(path = FileLocation, pattern = 'GatesUnmixed.csv'))
+#' UnmixedGating <- gatingTemplate(UnmixedGates)
+#' gt_gating(UnmixedGating, UnmixedGatingSet)
+#'
+#' removestrings <-  c("DTR_", ".fcs")
+#' StorageLocation <- file.path("C:", "Users", "JohnDoe", "Desktop")
+#'
+#' Markers <- colnames(UnmixedCytoSet)
+#' KeptMarkers <- Markers[-grep("Time|FS|SC|SS|Original|-W$|-H$|AF", Markers)]
+#' SubsetMarkers <- c("BUV496-A", "BUV805-A", "Pacific Blue-A", "BV711-A",
+#'   "BV786-A", "Spark Blue 550-A", "PE-A", "APC-Fire 750-A")
+#'
+#' tSNE_Output <- Utility_tSNE(x=UnmixedGatingSet[[1]], sample.name = "TUBENAME",
+#'  removestrings=removestrings, subset = "live", columns=SubsetMarkers, export=FALSE)
+#'
 Utility_tSNE <- function(x, sample.name, removestrings, subset, columns=NULL,
   notcolumns=NULL, subsample=NULL, export=FALSE, outpath=NULL, max_iter = 1000,
   perplexity = 30, ...){
---
+++ tests/testthat/test-Utility_Downsample.R
@@ -0,0 +1,46 @@
+test_that("Utility_Downsample returns a flowframe with correct number rows", {
+  # Prepare the experiment
+  library(BiocGenerics)
+  library(flowCore)
+  library(flowWorkspace)
+  library(openCyto)
+  library(data.table)
+
+  File_Location <- system.file("extdata", package = "Luciernaga")
+  FCS_Files <- list.files(path = File_Location, pattern = ".fcs",
+                          full.names = TRUE)
+  Unmixed_FullStained <- FCS_Files[grep("Unmixed", FCS_Files)]
+  UnmixedFCSFiles <- Unmixed_FullStained[1]
+  UnmixedCytoSet <- load_cytoset_from_fcs(UnmixedFCSFiles[1],
+                                          truncate_max_range = FALSE,
+                                          transformation = FALSE)
+  UnmixedGatingSet <- GatingSet(UnmixedCytoSet)
+  Markers <- colnames(UnmixedCytoSet)
+  KeptMarkers <- Markers[-grep("Time|FS|SC|SS|Original|-W$|-H$|AF", Markers)]
+  MyBiexponentialTransform <- flowjo_biexp_trans(channelRange = 256,
+                                                 maxValue = 1000000,
+                                                 pos = 4.5, neg = 0,
+                                                 widthBasis = -1000)
+  TransformList <- transformerList(KeptMarkers, MyBiexponentialTransform)
+  UnmixedGatingSet <- flowWorkspace::transform(UnmixedGatingSet, TransformList)
+  FileLocation <- system.file("extdata", package = "Luciernaga")
+  UnmixedGates <- fread(file.path(path = FileLocation, pattern = 'GatesUnmixed.csv'))
+  UnmixedGating <- gatingTemplate(UnmixedGates)
+  gt_gating(UnmixedGating, UnmixedGatingSet)
+  removestrings <-  c("DTR_", ".fcs")
+  StorageLocation <- file.path("C:", "Users", "JohnDoe", "Desktop")
+
+  CountData <- gs_pop_get_count_fast(UnmixedGatingSet)
+  Counts_Specimen <- CountData %>%
+    filter(Population %in% "/singletsFSC/singletsSSC/singletsSSCB/nonDebris/lymphocytes/live") %>%
+    select(name, Count)
+
+  # Execute the experiment
+  SingleSample <- Utility_Downsample(UnmixedGatingSet[1], sample.name = "GROUPNAME",
+                                     removestrings=removestrings, subsets = "live",
+                                     subsample = 2500, internal = FALSE, export = FALSE)
+
+  # Did it return a patchwork object?
+  expect_equal(nrow(SingleSample), 2500)
+})
+
