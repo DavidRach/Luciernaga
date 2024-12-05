d96e03bdd4b1c6e5e6ee78a0a59a770293468e8a
David Rach <drach@som.umaryland.edu>
2024-12-05 11:58:11 -0500

    Ability to pass list fluorophores for reference plots

--- R/QC_ReferenceLibrary.R
+++ R/QC_ReferenceLibrary.R
@@ -5,6 +5,7 @@
 #' For Cytek Instruments 5L = 64, 4L_UV = 54, 4L_YG = 48, 3L=38, 2L_VB=30,
 #' 2L_BR=22, 1L=14
 #' @param returnPlots Whether to return signature plot as well. Default FALSE.
+#' @param ListOverride Default FALSE
 #'
 #' @importFrom dplyr select
 #' @importFrom dplyr filter
@@ -15,14 +16,19 @@
 #'
 #' @examples
 #' QC_ReferenceLibrary(FluorNameContains = "FITC", NumberDetectors=64)
-QC_ReferenceLibrary <- function(FluorNameContains, NumberDetectors, returnPlots=FALSE){
+QC_ReferenceLibrary <- function(FluorNameContains, NumberDetectors,
+                                returnPlots=FALSE, ListOverride=FALSE){
   ReferenceData <- InstrumentReferences(NumberDetectors=NumberDetectors)
   if (returnPlots == TRUE){ReferenceData1 <- ReferenceData}
+
 
   TheList <- ReferenceData %>% select(Fluorophore) %>% unique()
   rownames(TheList) <- NULL
 
+  if (ListOverride == FALSE){
   Subset <- TheList %>% filter(str_detect(Fluorophore, FluorNameContains))
+  } else {Subset <- TheList %>% dplyr::filter(
+    Fluorophore %in% FluorNameContains)}
 
   if (returnPlots==TRUE){
     TheseFluorophores <- Subset %>% pull(Fluorophore)
