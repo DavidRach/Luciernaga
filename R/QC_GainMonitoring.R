7c0f2ea586ac359a1172262223b2369097b5d992
David Rach <drach@som.umaryland.edu>
2025-04-12 23:06:19 -0400

    Added the parse to QC_GainMonitoring to return the %RCV.

--- R/QC_GainMonitoring.R
+++ R/QC_GainMonitoring.R
@@ -7,12 +7,15 @@
 #' @param subsets When provided with a GatingSet, selects this subset to provide the Cytoset
 #' @param inverse.transform Default is FALSE. 
 #'
+#' @importFrom flowWorkspace gs_pop_get_data
 #' @importFrom Biobase exprs
+#' @importFrom purrr map
+#' @importFrom dplyr bind_cols
 #' @importFrom dplyr select
-#' @importFrom stringr str_detect
 #' @importFrom dplyr mutate
 #' @importFrom dplyr relocate
-#' @importFrom flowWorkspace gs_pop_get_data
+#' @importFrom stringr str_detect
+#' 
 #'
 #' @return A data.frame row
 #' @export
@@ -41,15 +44,23 @@ inverse.transform=FALSE){
 
   Guts <- QC_Retrieval(x=x, sample.name=sample.name)
   Data <- data.frame(exprs(x), check.names=FALSE)
+
+  These <- colnames(Data)
+  These <- These[These != "Time"]
+  TheRCVs <- map(.x=These, .f=InternalRCV, data=Data) |>
+        bind_cols()
+  TheRCVs <- round(TheRCVs*100, 2)
+  colnames(TheRCVs) <- paste0(colnames(TheRCVs), "-% rCV")
+
   Data <- AveragedSignature(Data, stats)
-  Data <- Data %>% select(-Time)
-  Bound <- cbind(Guts, Data)
+  Data <- Data |> select(-Time)
+  Bound <- cbind(Guts, TheRCVs, Data)
   Bound[["SAMPLE"]] <- NameCleanUp(Bound[["SAMPLE"]], removestrings = ".fcs")
   Bound[["SAMPLE"]] <- NameCleanUp(Bound[["SAMPLE"]], removestrings = ".fcs")
   if (str_detect(Bound[["SAMPLE"]], "efore")){
-    Bound <- Bound %>% mutate(Timepoint = "Before") %>% relocate(Timepoint, .after=TIME)
+    Bound <- Bound |> mutate(Timepoint = "Before") |> relocate(Timepoint, .after=TIME)
   } else if (str_detect(Bound[["SAMPLE"]], "fter")){
-    Bound <- Bound %>% mutate(Timepoint = "After") %>% relocate(Timepoint, .after=TIME)
-  } else {Bound <- Bound %>% mutate(Timepoint = "Unknown") %>% relocate(Timepoint, .after=TIME)}
+    Bound <- Bound |> mutate(Timepoint = "After") |> relocate(Timepoint, .after=TIME)
+  } else {Bound <- Bound |> mutate(Timepoint = "Unknown") |> relocate(Timepoint, .after=TIME)}
   return(Bound)
 }
