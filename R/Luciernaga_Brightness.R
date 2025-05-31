73ee638fefe01b7535b4b17c6e3ed0a4c2e641b0
David Rach <drach@som.umaryland.edu>
2025-05-31 08:13:07 -0400

    Update to the Brightness functions

--- R/Luciernaga_Brightness.R
+++ R/Luciernaga_Brightness.R
@@ -1,84 +1,115 @@
 #' Visualize MFI of raw .fcs files to evaluate single color controls.
 #'
-#' @param x A passed Fluorophore name corresponding to those found
-#'  in the data.
-#' @param data A data.frame containing the Luciernaga_FCSToReport
-#'  raw MFI values.
-#' @param Downsample Whether to Downsample, default is set to TRUE
-#'  to not overly
-#' affect the y-axis
-#' @param subsample If desired, a certain number of cells to take 
-#' from each cluster.
-#' Default is NULL, selecting the number of cells found in the 
-#' smallest cluster.
+#' @param fluorophore.name The name of the fluorophore to filter from data
+#' @param data The data.frame derrived by Luciernaga_FolderBrightness, consisting
+#' of fluorophore and cluster metadata, and the raw detector values
+#' @param fluorophore.column The name of the column containing the fluorophore 
+#' information
+#' @param cluster.column The name of the column containing the cluster information
+#' @param downsample Default is TRUE, to avoid having large populations take over
+#' the entire y-axis.
+#' @param subsample When downsample is true, number of cells from each cluster to
+#' keep. The default NULL will select the number of cells found in the smallest cluster
+#' @param Detector Default NULL, when reference is NULL sets the detector to plot
+#' on the x-axis
 #' @param reference A .csv path or data.frame containing Fluorophore
-#'  and Detector info.
+#' and Detector information from which to retrieve the x-axis detector, use same 
+#' naming as with the passed fluorophore argument in x
 #' @param clearance A buffer area multiplier to xmin and xmax when
-#'  Scaled equals False
-#' @param Scaled Default is set to TRUE, returning logicle 
-#' transformed MFI. FALSE returns raw MFI.
+#'  scaled equals false
+#' @param Scaled Default is set to TRUE, returning log_10 transformed data, 
+#' FALSE will return raw MFI.
+#' @param maxtik Default is 1e6
+#' @param legend Default is "right"
 #'
 #' @importFrom dplyr filter
 #' @importFrom dplyr arrange
+#' @importFrom utils head 
 #' @importFrom dplyr pull
 #' @importFrom dplyr group_by
 #' @importFrom dplyr slice_sample
 #' @importFrom dplyr ungroup
-#' @importFrom dplyr select
-#' @importFrom ggplot2 ggplot
-#' @importFrom ggcyto scale_x_logicle
-#' @importFrom utils head
 #' @importFrom utils read.csv
+#' @importFrom dplyr pull
+#' @importFrom dplyr select
+#' @importFrom tidyselect where
+#' @importFrom stats quantile
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 geom_density
+#' @importFrom ggplot2 scale_x_log10
+#' @importFrom scales trans_format
+#' @importFrom scales math_format
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 theme_bw
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 element_text
 #'
 #' @return ggplot objects for each fluorophore containing the various clusters
 #' @export
 #'
 #' @examples NULL
-Luciernaga_Brightness <- function(x, data, Downsample=TRUE, subsample = NULL,
-                                  reference, clearance=0.02, Scaled = TRUE){
-    TheFluorophore <- x
-    TheData <- data |> filter(Sample %in% x)
-    TheTable <- data.frame(table(TheData$Cluster), check.names = FALSE)
+Luciernaga_Brightness <- function(fluorophore.name, data,
+  fluorophore.column, cluster.column, downsample=TRUE,
+  subsample = NULL, detector=NULL, reference=NULL, 
+  clearance=0.02, Scaled = TRUE, maxtik=1e6, legend="right"){
+  
+    TheFluorophore <- fluorophore.name
+    TheData <- data |> filter(.data[[fluorophore.column]] %in% TheFluorophore)
+    TheTable <- data.frame(table(TheData[[cluster.column]]), check.names = FALSE)
     colnames(TheTable)[1] <- "Cluster"
     colnames(TheTable)[2] <- "Count"
     TheSlice <- TheTable |> arrange(Count) |> head(1) |> pull(Count)
 
-    if (Downsample == TRUE) {
+    if (downsample == TRUE) {
       if (is.null(subsample)){
-       TheData <- TheData |> group_by(Cluster) |>
+       TheData <- TheData |> group_by(.data[[cluster.column]]) |>
          slice_sample(n=TheSlice, replace = FALSE) |> ungroup()
-      } else {TheData <- TheData |> group_by(Cluster) |>
+      } else {TheData <- TheData |> group_by(.data[[cluster.column]]) |>
         slice_sample(n=subsample, replace = FALSE) |> ungroup()
       }
     }
 
-    if (!is.data.frame(reference)){CSV <- read.csv(reference, check.names = FALSE)
+    if (is.null(reference)){TheDetector <- detector
+    } else {
+      if(!is.data.frame(reference)){
+        CSV <- read.csv(reference, check.names = FALSE)
       } else {CSV <- reference}
-
     internalstrings <- c(" ", ".", "_", "-A")
     CSV$Fluorophore <- NameCleanUp(CSV$Fluorophore, removestrings=internalstrings)
     CSV$Detector <- NameCleanUp(CSV$Detector, removestrings=internalstrings)
-    TheDetector <- CSV %>% dplyr::filter(Fluorophore %in% TheFluorophore) |>
+    TheDetector <- CSV |> dplyr::filter(Fluorophore %in% TheFluorophore) |>
       pull(Detector)
+    }
 
-    Values <- TheData |> select(all_of(TheDetector)) |> as.matrix()
+    Values <- TheData |> select(where(is.numeric)) |> as.matrix()
     theXmin <- Values %>% quantile(., 0.01)
     theXmax <- Values %>% quantile(., 0.99)
     theXmin <- theXmin - abs((clearance*theXmin))
     theXmax <- theXmax + (clearance*theXmax)
 
     if (Scaled == TRUE){
-      plot <- ggplot(TheData, aes(x=.data[[TheDetector]], fill=Cluster)) +
-        geom_density(alpha=0.5) + scale_x_logicle(w=1.5, t=4200000, m=5.62) +
+      if (maxtik == 1e6){
+        custom_breaks <- c(1e3, 1e4, 1e5, 1e6)
+        HighEnd <- 1e6
+      } else {custom_breaks <- c(1e3, 1e4, 1e5, maxtik)
+      HighEnd <- maxtik
+      }
+      
+
+      plot <- ggplot(TheData, aes(x=.data[[TheDetector]],
+         fill=.data[[cluster.column]])) + geom_density(alpha=0.5) +
+          scale_x_log10(limits = c(1e3, HighEnd), breaks = custom_breaks, 
+        labels = scales:::trans_format("log10", scales::math_format(10^.x)))  +
         labs(title=TheFluorophore, x=TheDetector, y="Frequency") + theme_bw() +
         theme(axis.title.x=element_text(face="plain"), axis.title.y=element_text(
-            face="plain"))
+            face="plain"), legend.position=legend)
       } else {
         plot <- ggplot(TheData, aes(x=.data[[TheDetector]], fill=Cluster)) +
           geom_density(alpha=0.5) + xlim(theXmin, theXmax) +
           labs(title=TheFluorophore, x=TheDetector, y="Frequency") + theme_bw() +
           theme(axis.title.x=element_text(face="plain"), axis.title.y=element_text(
-              face="plain"))
+              face="plain"), legend.position=legend)
     }
 
  return(plot)
--- R/Luciernaga_FolderBrightness.R
+++ R/Luciernaga_FolderBrightness.R
@@ -1,7 +1,37 @@
 #' Sister function to FolderSignature, returns a density plot of brightness
+#'
+#' @param FolderPath Location where the Luciernaga_QC .fcs file
+#' outputs are stored
+#' @param sample.name Keyword in .fcs corresponding samples name
+#' @param StringRemoval Default NULL, remove these from the sample name
+#' @param fluorophore.name The name of the fluorophore
+#' @param Verbose Default FALSE
+#' @param stats Default is median
+#' @param PanelCuts Default is NULL
+#' @param normalize Default is FALSE, will scale to peak detector
+#' @param detector The detector corresponding to the fluorophore,
+#' sets the x-axis
+#' @param returnType Default data, providing plot returns a 
+#' Luciernaga_Brightness plot
+#' @param maxtick Default 1e6, for plot return
+#' @param legend Default is right, set to none to remove.
+#' 
+#' @importFrom flowWorkspace load_cytoset_from_fcs
+#' @importFrom flowWorkspace GatingSet
+#' @importFrom purrr map
+#' @importFrom dplyr bind_rows
+#' 
+#' @export
+#' 
+#' @return Either a data.frame of brightness values, or a plot
+#' 
+#' @examples
+#' A <- 2+2
+#' 
 Luciernaga_FolderBrightness <- function(FolderPath, sample.name,
   StringRemoval=NULL, fluorophore.name, Verbose=FALSE,
-   stats="median", PanelCuts=NULL, normalize=TRUE){
+   stats="median", PanelCuts=NULL, normalize=FALSE, detector,
+  returnType = "data", maxtick=1e6, legend="right"){
 
   TheFCSFiles <- list.files(path=FolderPath, pattern="fcs",
    full.names=TRUE) 
@@ -15,5 +45,15 @@ Luciernaga_FolderBrightness <- function(FolderPath, sample.name,
     stats=stats, PanelCuts=PanelCuts, normalize=FALSE,
     returnType="Brightness") |> bind_rows()
   
-  return(Returns)
+  if (returnType == "data"){
+    return(Returns)
+  } else {
+    BrightnessPlot <- Luciernaga_Brightness(data=Returns,
+      fluorophore.name=fluorophore.name,
+      fluorophore.column="Fluorophore", cluster.column="Sample",
+      downsample=TRUE, subsample = NULL, detector=detector,
+      reference=NULL, clearance=0.02, Scaled = TRUE, maxtik=maxtik,
+      legend=legend)    
+    return(BrightnessPlot)
+  }
 }
